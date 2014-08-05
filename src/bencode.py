5ca7e7d22567a9276439e5f0b71d88cf7c8814c2
mrmorgan <bimspam@mail.ru>
2014-08-05 11:18:09 +0600

    Bencode

--- src/bencode.py
+++ src/bencode.py
@@ -13,29 +13,42 @@ class Bencode():
         return True
 
     def Read(self):
-        """Converts Bencode file contents to dictionary"""
+        """Converts Bencode file contents to element, generally dictionary"""
         if not self._File:
-            return {}
+            return None
         self._Data = self._ReadElement()
         return self._Data
         
     def _ReadNumber(self):
         """Format: i<Digits>e"""
-        return 0
+        try:
+            Type = self._ReadByte()
+            if Type != "i":
+                raise RuntimeError("Element is not a number")
+            Number = self._ReadDigits()
+            self._ReadByte()
+            return Number
+        except:
+            return 0
 
     def _ReadByteArray(self):
         """Format: <Array Size>:<Array Bytes>"""
         try:
-            Byte = ""
-            Size = ""
-            while Byte != ":":
-                Size += Byte
-                Byte = self._ReadByte()
-            Size = int(Size)
+            Type = self._ReadByte(True)
+            if not Type in self._DigitsGenerator():
+                raise RuntimeError("Element is not a byte array")
+            Size = self._ReadDigits()
+            self._ReadByte()
             ByteArray = ""
             for i in xrange(Size):
-                Byte = self._ReadByte()
-                ByteArray += Byte
+                try:
+                    Byte = self._ReadByte()
+                    ByteArray += Byte
+                    print len(Byte)
+                except:
+                    print "Err: %d" % i
+            if Size == 11740:
+                print "%d (%d)" % (Size, len(ByteArray))
             return ByteArray
         except:
             return ""
@@ -45,7 +58,16 @@ class Bencode():
         try:
             Type = self._ReadByte()
             if Type != "l":
-                raise Exception()
+                raise RuntimeError("Element is not a list")
+            List = []
+            while True:
+                Byte = self._ReadByte(True)
+                if Byte == "e":
+                    break
+                Element = self._ReadElement()
+                List.append(Element)
+            self._ReadByte()
+            return List
         except:
             return []
 
@@ -55,21 +77,28 @@ class Bencode():
         try:
             Type = self._ReadByte()
             if Type != "d":
-                raise Exception()
+                raise RuntimeError("Element is not a dictionary")
             Dictionary = {}
-            ByteArray = self._ReadByteArray()
-            Element = self._ReadElement()
-            if Element == False:
-                raise Exception()
-            Dictionary[ByteArray] = Element
+            # TODO: delete this
+            max = 100
+            while True:
+                Byte = self._ReadByte(True)
+                if Byte == "e":
+                    break
+                Key = self._ReadByteArray()
+                Value = self._ReadElement()
+                Dictionary[Key] = Value
+                max -= 1
+                if max == 0:
+                    break
+            self._ReadByte()
             return Dictionary
         except:
             return {}
 
     def _ReadElement(self):
         """Automatic type recognizing"""
-        Type = self._ReadByte()
-        self._ReturnCursor()
+        Type = self._ReadByte(True)
         if Type == "i":
             Number = self._ReadNumber()
             return Number
@@ -84,11 +113,26 @@ class Bencode():
             return ByteArray
         return None
 
-    def _ReadByte(self):
-        return self._File.read(1)
+    def _ReadByte(self, quiet=False):
+        Byte = self._File.read(1)
+        if quiet:
+            self._File.seek(-1, 1)
+        return Byte
 
-    def _ReturnCursor(self):
-        self._File.seek(-1, 1)
-    
+    def _ReadDigits(self):
+        Digits = ""
+        try:
+            while True:
+                Byte = self._ReadByte()
+                if Byte in self._DigitsGenerator():
+                    Digits += Byte
+                else:
+                    self._File.seek(-1, 1)
+                    break
+            Digits = int(Digits)
+        except:
+            Digits = 0
+        return Digits
+
     def _DigitsGenerator(self):
         return [str(x) for x in xrange(10)]
--- src/main.py
+++ src/main.py
@@ -4,12 +4,17 @@
 Console Bit Torrent Client
 """
 
+import sys
 from bencode import Bencode
 
 def main():
+    sys.dont_write_bytecode = True
     Object = Bencode()
     Object.Open("1.torrent")
-    print Object.Read()
+#    Object.Open("test.torrent")
+    dict = Object.Read()
+#    for key in dict:
+#        print "%s : %s" % (key, dict[key])
 
 if __name__ == "__main__":
     main()
